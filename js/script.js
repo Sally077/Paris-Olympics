59ab437388cd7c98a26ca665bccbe5ef7ff0e21c
Sally077 <sallyhedges77@gmail.com>
2023-02-15 20:11:42 +0000

    added polished readme and modal updates

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ function test() {
     // Retriving data for local storage
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-
+    
     // Storing data
 
     var name = localStorage.setItem("name", name);
     var email = localStorage.setItem("email", email);
 
 }
+
+$('#send').click(function() {
+    $('#modalSubscriptionForm').modal('hide');
+});
 
 
 // Hotels API
